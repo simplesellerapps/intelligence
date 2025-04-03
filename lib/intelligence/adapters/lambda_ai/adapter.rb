f973552ce20c0d293ee19f946a2cfb9713b219dc
guolei <guolei9@gmail.com>
2025-04-03 14:10:02 +0800

    update lambda_ai

---
+++ lib/intelligence/adapters/lambda_ai/adapter.rb
@@ -0,0 +1,85 @@
+require_relative 'chat_request_methods'
+require_relative 'chat_response_methods'
+
+module Intelligence
+  module LambdaAi
+    class Adapter < Adapter::Base
+
+      schema do
+
+        # normalized properties for all endpoints
+        key                         String
+
+        # openai properties for all endpoints
+        organization                String
+        project                     String
+
+        # properties for generative text endpoints
+        chat_options do
+
+          # normalized properties for openai generative text endpoint
+          model                     String, requried: true
+          n                         Integer
+          max_tokens                Integer, as: :max_completion_tokens
+          temperature               Float
+          top_p                     Float
+          seed                      Integer
+          stop                      String, array: true
+          stream                    [ TrueClass, FalseClass ]
+
+          frequency_penalty         Float
+          presence_penalty          Float
+
+          # openai variant of normalized properties for openai generative text endpoints
+          max_completion_tokens     Integer
+
+          # openai properties for openai generative text endpoint
+          audio do
+            voice                   String
+            format                  String
+          end
+          logit_bias
+          logprobs                  [ TrueClass, FalseClass ]
+          top_logprobs              Integer
+          modalities                String, array: true
+          response_format do
+            # 'text' and 'json_schema' are the only supported types
+            type                    Symbol, in: [ :text, :json_schema ]
+            json_schema
+          end
+          service_tier              String
+          stream_options do
+            include_usage           [ TrueClass, FalseClass ]
+          end
+          user
+
+          # open ai tools
+          tool                      array: true, as: :tools, &Tool.schema
+          # open ai tool choice configuration
+          #
+          # `tool_choice :none`
+          # or
+          # ```
+          # tool_choice :function do
+          #   function :my_function
+          # end
+          # ```
+          tool_choice               arguments: :type do
+            type                    Symbol, in: [ :none, :auto, :required ]
+            function                arguments: :name do
+              name                  Symbol
+            end
+          end
+          # the parallel_tool_calls parameter is only allowed when 'tools' are specified
+          parallel_tool_calls       [ TrueClass, FalseClass ]
+
+        end
+
+      end
+
+      include ChatRequestMethods
+      include ChatResponseMethods
+
+    end
+  end
+end
---
+++ lib/intelligence/adapters/lambda_ai/chat_response_methods.rb
@@ -0,0 +1,239 @@
+module Intelligence
+  module LambdaAi
+    module ChatResponseMethods
+
+      def chat_result_attributes( response )
+        return nil unless response.success?
+        response_json = JSON.parse( response.body, symbolize_names: true ) rescue nil
+        return nil \
+          if response_json.nil? || response_json[ :choices ].nil?
+
+        result = {}
+        result[ :choices ] = []
+
+        ( response_json[ :choices ] || [] ).each do | json_choice |
+          json_message = json_choice[ :message ]
+          result_message = nil
+          if ( json_message )
+            result_message = { role: json_message[ :role ] }
+            if json_message[ :content ]
+              result_message[ :contents ] = [ { type: :text, text: json_message[ :content ] } ]
+            end
+            if json_message[ :tool_calls ] && !json_message[ :tool_calls ].empty?
+              result_message[ :contents ] ||= []
+              json_message[ :tool_calls ].each do | json_message_tool_call |
+                result_message_tool_call_parameters =
+                  JSON.parse( json_message_tool_call[ :function ][ :arguments ], symbolize_names: true ) \
+                    rescue json_message_tool_call[ :function ][ :arguments ]
+                result_message[ :contents ] << {
+                  type: :tool_call,
+                  tool_call_id: json_message_tool_call[ :id ],
+                  tool_name: json_message_tool_call[ :function ][ :name ],
+                  tool_parameters: result_message_tool_call_parameters
+                }
+              end
+            end
+          end
+          result[ :choices ].push( {
+            end_reason: translate_end_result( json_choice[ :finish_reason ] ),
+            message: result_message
+          } )
+        end
+
+        metrics_json = response_json[ :usage ]
+        unless metrics_json.nil?
+
+          metrics = {}
+          metrics[ :input_tokens ] = metrics_json[ :prompt_tokens ]
+          metrics[ :output_tokens ] = metrics_json[ :completion_tokens ]
+          metrics = metrics.compact
+
+          result[ :metrics ] = metrics unless metrics.empty?
+
+        end
+
+        result
+      end
+
+      def chat_result_error_attributes( response )
+        error_type, error_description = translate_error_response_status( response.status )
+        result = {
+          error_type: error_type.to_s,
+          error_description: error_description
+        }
+
+        parsed_body = JSON.parse( response.body, symbolize_names: true ) rescue nil
+        if parsed_body && parsed_body.respond_to?( :include? ) && parsed_body.include?( :error )
+          result = {
+            error_type: error_type.to_s,
+            error: parsed_body[ :error ][ :code ] || error_type.to_s,
+            error_description: parsed_body[ :error ][ :message ] || error_description
+          }
+        end
+
+        result
+      end
+
+      def stream_result_chunk_attributes( context, chunk )
+        context ||= {}
+        buffer = context[ :buffer ] || ''
+        metrics = context[ :metrics ] || {
+          input_tokens: 0,
+          output_tokens:  0
+        }
+        choices = context[ :choices ] || Array.new( 1 , { message: {} } )
+
+        choices.each do | choice |
+          choice[ :message ][ :contents ] = choice[ :message ][ :contents ]&.map do | content |
+            { type: content[ :type ] }
+          end
+        end
+
+        buffer += chunk
+        while ( eol_index = buffer.index( "\n" ) )
+          line = buffer.slice!( 0..eol_index )
+          line = line.strip
+          next if line.empty? || !line.start_with?( 'data:' )
+          line = line[ 6..-1 ]
+
+          next if line.end_with?( '[DONE]' )
+          data = JSON.parse( line ) rescue nil
+
+          if data.is_a?( Hash )
+            data[ 'choices' ]&.each do | data_choice |
+
+              data_choice_index = data_choice[ 'index' ]
+              data_choice_delta = data_choice[ 'delta' ]
+              data_choice_finish_reason = data_choice[ 'finish_reason' ]
+
+              choices.fill( { message: {} }, choices.size, data_choice_index + 1 ) \
+                if choices.size <= data_choice_index
+              contents = choices[ data_choice_index ][ :message ][ :contents ] || []
+
+              text_content = contents.first&.[]( :type ) == :text ? contents.first : nil
+              if data_choice_content = data_choice_delta[ 'content' ]
+                if text_content.nil?
+                  contents.unshift( text_content = { type: :text, text: data_choice_content } )
+                else
+                  text_content[ :text ] = ( text_content[ :text ] || '' ) + data_choice_content
+                end
+              end
+              if data_choice_tool_calls = data_choice_delta[ 'tool_calls' ]
+                data_choice_tool_calls.each do | data_choice_tool_call |
+                  if data_choice_tool_call_function = data_choice_tool_call[ 'function' ]
+                    data_choice_tool_index = data_choice_tool_call[ 'index' ]
+                    data_choice_tool_id = data_choice_tool_call[ 'id' ]
+                    data_choice_tool_name = data_choice_tool_call_function[ 'name' ]
+                    data_choice_tool_parameters = data_choice_tool_call_function[ 'arguments' ]
+
+                    tool_call_content_index = ( text_content.nil? ? 0 : 1 ) + data_choice_tool_index
+                    if tool_call_content_index >= contents.length
+                      contents.push( {
+                        type: :tool_call,
+                        tool_call_id: data_choice_tool_id,
+                        tool_name: data_choice_tool_name,
+                        tool_parameters: data_choice_tool_parameters
+                      } )
+                    else
+                      tool_call = contents[ tool_call_content_index ]
+                      tool_call[ :tool_call_id ] = ( tool_call[ :tool_call_id ] || '' ) + data_choice_tool_id \
+                        if data_choice_tool_id
+                      tool_call[ :tool_name ] = ( tool_call[ :tool_name ] || '' ) + data_choice_tool_name \
+                        if data_choice_tool_name
+                      tool_call[ :tool_parameters ] = ( tool_call[ :tool_parameters ] || '' ) + data_choice_tool_parameters \
+                        if data_choice_tool_parameters
+                    end
+                  end
+                end
+              end
+              choices[ data_choice_index ][ :message ][ :contents ] = contents
+              choices[ data_choice_index ][ :end_reason ] ||=
+                translate_end_result( data_choice_finish_reason )
+            end
+
+            if usage = data[ 'usage' ]
+              metrics[ :input_tokens ] += usage[ 'prompt_tokens' ]
+              metrics[ :output_tokens ] += usage[ 'completion_tokens' ]
+            end
+
+          end
+
+        end
+
+        context[ :buffer ] = buffer
+        context[ :metrics ] = metrics
+        context[ :choices ] = choices
+
+        [ context, choices.empty? ? nil : { choices: choices.dup } ]
+      end
+
+      def stream_result_attributes( context )
+        choices = context[ :choices ]
+        metrics = context[ :metrics ]
+
+        choices = choices.map do | choice |
+          { end_reason: choice[ :end_reason ] }
+        end
+
+        { choices: choices, metrics: context[ :metrics ] }
+      end
+
+      alias_method :stream_result_error_attributes, :chat_result_error_attributes
+
+    private
+
+      def translate_end_result( end_result )
+        case end_result
+          when 'stop'
+            :ended
+          when 'length'
+            :token_limit_exceeded
+          when 'tool_calls'
+            :tool_called
+          when 'content_filter'
+            :filtered
+          else
+            nil
+        end
+      end
+
+      def translate_error_response_status( status )
+        case status
+        when 400
+          [ :invalid_request_error,
+            "There was an issue with the format or content of your request." ]
+        when 401
+          [ :authentication_error,
+            "There's an issue with your API key." ]
+        when 403
+          [ :permission_error,
+            "Your API key does not have permission to use the specified resource." ]
+        when 404
+          [ :not_found_error,
+            "The requested resource was not found." ]
+        when 413
+          [ :request_too_large,
+            "Request exceeds the maximum allowed number of bytes." ]
+        when 422
+          [ :invalid_request_error,
+            "There was an issue with the format or content of your request." ]
+        when 429
+          [ :rate_limit_error,
+            "Your account has hit a rate limit." ]
+        when 500, 502, 503
+          [ :api_error,
+            "An unexpected error has occurred internal to the providers systems." ]
+        when 529
+          [ :overloaded_error,
+            "The providers server is temporarily overloaded." ]
+        else
+          [ :unknown_error, "
+            An unknown error occurred." ]
+        end
+      end
+
+    end
+
+  end
+
+end
